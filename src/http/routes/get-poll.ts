b964d4251a75e63176d32e296a36dc2faf9fbf5d
mariocesar33 <mcgato33@hotmail.com>
2024-02-11 01:22:44 -0100

    adicionar o Redis para lidar com o cache e implementar a funcionalidade de realtime utilizando websockets

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import z from "zod"
 
 import { prisma } from "../../lib/prisma"
+import { redis } from "../../lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -29,7 +30,52 @@ export async function getPoll(app: FastifyInstance) {
         },
       }
     })
+
+    if (!poll) {
+      return reply.status(400).send({ message: "poll not found." })
+    }
+
+    // -1 => quer dizer que eu quero todas
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    //console.log(result) 
+    // [
+    //  '81869c79-7411-481e-b41d-0189cede3645',
+    //  '0',
+    //  '6ba1b27b-facf-4449-886b-ac946460c127',
+    //  '1'
+    // ]
+
+    // converter um array para chave e valor
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    console.log(votes)
+    // {
+    //   '81869c79-7411-481e-b41d-0189cede3645': 0,
+    //   '6ba1b27b-facf-4449-886b-ac946460c127': 1
+    // }
   
-    return reply.send({ poll })
+    // este conteudo esta na aula 3 no minuti 24:25
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: (option.id in votes) ? votes[option.id] : 0,
+          }
+        })
+      }
+    })
   })
 }
